085996d3dc6513e6097221fed9e58b42797aeac6
Wilfrantz <wilfrantz@outlook.com>
2020-04-12 17:21:43 +0100

    Application entry point (main function) file

---
+++ wish.c
@@ -0,0 +1,17 @@
+
+/* wish.c:
+ * wish (Wilfrantz Shell) Simple Linux Command Line Shell.
+ * April 2020
+ * */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+	while (true){
+		printf("> ");
+	}
+
+	return 0;
+}
